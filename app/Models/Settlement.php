34b3de7fea8a819a0a0cc1b594e7b5b72eef276b
saraabbih1 <abbihsara30@gmail.com>
2026-02-24 22:41:04 +0000

    feat:categorie et sttlement model

--- app/Models/Settlement.php
+++ app/Models/Settlement.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Settlement extends Model
 {
-    //
+     protected $fillable = [
+        'colocation_id',
+        'debtor_id',
+        'creditor_id',
+        'amount',
+        'status'
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function debtor()
+    {
+        return $this->belongsTo(User::class, 'debtor_id');
+    }
+
+    public function creditor()
+    {
+        return $this->belongsTo(User::class, 'creditor_id');
+    }
 }
