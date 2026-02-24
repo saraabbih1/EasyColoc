8a5a580888d88097d4bc956a1aa3e8c949bbecb6
saraabbih1 <abbihsara30@gmail.com>
2026-02-24 22:39:52 +0000

    feat:expense model

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
-}
+ protected $fillable = [
+        'title',
+        'amount',
+        'date',
+        'user_id',
+        'colocation_id',
+        'category_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }}
