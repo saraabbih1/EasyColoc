34911bce33245e5c9d702d13ee8289fa1ecb2d24
saraabbih1 <abbihsara30@gmail.com>
2026-02-24 22:38:48 +0000

    feat:membership model

--- app/Models/Membership.php
+++ app/Models/Membership.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Membership extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'colocation_id',
+        'role',
+        'status',
+        'joined_at',
+        'left_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
 }
