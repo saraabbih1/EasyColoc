be64e97ea8f1ec189c1e077cc03f2bac76d6ca62
saraabbih1 <abbihsara30@gmail.com>
2026-03-03 15:07:10 +0000

    update: database/migrations/2026_02_23_112107_create_invitations_table.php

--- database/migrations/2026_02_23_112107_create_invitations_table.php
+++ database/migrations/2026_02_23_112107_create_invitations_table.php
@@ -16,8 +16,13 @@ return new class extends Migration
             $table->string('email');
             $table->string('token')->unique();
             $table->foreignId('colocation_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('invited_by')->constrained('users')->cascadeOnDelete();
             $table->enum('status', ['pending', 'accepted', 'refused'])->default('pending');
+            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
+            
+            $table->index(['email', 'status']);
+            $table->index(['token']);
         });
     }
 
